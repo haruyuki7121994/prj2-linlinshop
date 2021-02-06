ab7e8637e21f18ee6ab6f62ce8c0bc2155dfbdf2
Dat <dat.quang09@gmail.com>
2021-02-06 11:38:20 +0700

    Create ProductAttributeSeeder2.php
    tao seeder product attr skirt, dress, jean

---
+++ database/seeds/ProductAttributeSeeder2.php
@@ -0,0 +1,117 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductAttributeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('product_attributes')->insert([
+            ['product_id' => 36, 'color_id' => 1, 'size' => 2, 'qty' => 10, 'price' => 120],
+            ['product_id' => 36, 'color_id' => 1, 'size' => 3, 'qty' => 10, 'price' => 120],
+            ['product_id' => 36, 'color_id' => 1, 'size' => 4, 'qty' => 10, 'price' => 120],
+
+            ['product_id' => 37, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 130],
+            ['product_id' => 37, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 130],
+            ['product_id' => 37, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 130],
+
+            ['product_id' => 38, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 120],
+            ['product_id' => 38, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 120],
+            ['product_id' => 38, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 120],
+
+            ['product_id' => 39, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 130],
+            ['product_id' => 39, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 130],
+            ['product_id' => 39, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 130],
+
+            ['product_id' => 40, 'color_id' => 4, 'size' => 2, 'qty' => 10, 'price' => 350],
+            ['product_id' => 40, 'color_id' => 4, 'size' => 3, 'qty' => 10, 'price' => 350],
+            ['product_id' => 40, 'color_id' => 4, 'size' => 4, 'qty' => 10, 'price' => 350],
+
+            ['product_id' => 41, 'color_id' => 10, 'size' => 2, 'qty' => 10, 'price' => 300],
+            ['product_id' => 41, 'color_id' => 10, 'size' => 3, 'qty' => 10, 'price' => 300],
+            ['product_id' => 41, 'color_id' => 10, 'size' => 4, 'qty' => 10, 'price' => 300],
+
+            ['product_id' => 42, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 250],
+            ['product_id' => 42, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 250],
+            ['product_id' => 42, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 250],
+
+            ['product_id' => 43, 'color_id' => 3, 'size' => 2, 'qty' => 10, 'price' => 230],
+            ['product_id' => 43, 'color_id' => 3, 'size' => 3, 'qty' => 10, 'price' => 230],
+            ['product_id' => 43, 'color_id' => 3, 'size' => 4, 'qty' => 10, 'price' => 230],
+
+            ['product_id' => 44, 'color_id' => 3, 'size' => 2, 'qty' => 10, 'price' => 200],
+            ['product_id' => 44, 'color_id' => 3, 'size' => 3, 'qty' => 10, 'price' => 200],
+            ['product_id' => 44, 'color_id' => 3, 'size' => 4, 'qty' => 10, 'price' => 200],
+
+            ['product_id' => 45, 'color_id' => 13, 'size' => 2, 'qty' => 10, 'price' => 100],
+            ['product_id' => 45, 'color_id' => 13, 'size' => 3, 'qty' => 10, 'price' => 100],
+            ['product_id' => 45, 'color_id' => 13, 'size' => 4, 'qty' => 10, 'price' => 100],
+
+            ['product_id' => 46, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 250],
+            ['product_id' => 46, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 250],
+            ['product_id' => 46, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 250],
+
+            ['product_id' => 47, 'color_id' => 5, 'size' => 2, 'qty' => 10, 'price' => 260],
+            ['product_id' => 47, 'color_id' => 5, 'size' => 3, 'qty' => 10, 'price' => 260],
+            ['product_id' => 47, 'color_id' => 5, 'size' => 4, 'qty' => 10, 'price' => 260],
+
+            ['product_id' => 48, 'color_id' => 5, 'size' => 2, 'qty' => 10, 'price' => 350],
+            ['product_id' => 48, 'color_id' => 5, 'size' => 3, 'qty' => 10, 'price' => 350],
+            ['product_id' => 48, 'color_id' => 5, 'size' => 4, 'qty' => 10, 'price' => 350],
+
+            ['product_id' => 49, 'color_id' => 1, 'size' => 2, 'qty' => 10, 'price' => 120],
+            ['product_id' => 49, 'color_id' => 1, 'size' => 3, 'qty' => 10, 'price' => 120],
+            ['product_id' => 49, 'color_id' => 1, 'size' => 4, 'qty' => 10, 'price' => 120],
+
+            ['product_id' => 50, 'color_id' => 3, 'size' => 2, 'qty' => 10, 'price' => 200],
+            ['product_id' => 50, 'color_id' => 3, 'size' => 3, 'qty' => 10, 'price' => 200],
+            ['product_id' => 50, 'color_id' => 3, 'size' => 4, 'qty' => 10, 'price' => 200],
+
+            ['product_id' => 50, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 470],
+            ['product_id' => 50, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 470],
+            ['product_id' => 50, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 470],
+
+            ['product_id' => 51, 'color_id' => 2, 'size' => 2, 'qty' => 10, 'price' => 200],
+            ['product_id' => 51, 'color_id' => 2, 'size' => 3, 'qty' => 10, 'price' => 200],
+            ['product_id' => 51, 'color_id' => 2, 'size' => 4, 'qty' => 10, 'price' => 200],
+
+
+            ['product_id' => 52, 'color_id' => 12, 'size' => 2, 'qty' => 10, 'price' => 200],
+            ['product_id' => 52, 'color_id' => 12, 'size' => 3, 'qty' => 10, 'price' => 200],
+            ['product_id' => 52, 'color_id' => 12, 'size' => 4, 'qty' => 10, 'price' => 200],
+
+            ['product_id' => 53, 'color_id' => 12, 'size' => 2, 'qty' => 10, 'price' => 400],
+            ['product_id' => 53, 'color_id' => 12, 'size' => 3, 'qty' => 10, 'price' => 400],
+            ['product_id' => 53, 'color_id' => 12, 'size' => 4, 'qty' => 10, 'price' => 400],
+
+            ['product_id' => 54, 'color_id' => 12, 'size' => 2, 'qty' => 10, 'price' => 400],
+            ['product_id' => 54, 'color_id' => 12, 'size' => 3, 'qty' => 10, 'price' => 400],
+            ['product_id' => 54, 'color_id' => 12, 'size' => 4, 'qty' => 10, 'price' => 400],
+
+            ['product_id' => 55, 'color_id' => 3, 'size' => 2, 'qty' => 10, 'price' => 200],
+            ['product_id' => 55, 'color_id' => 3, 'size' => 3, 'qty' => 10, 'price' => 200],
+            ['product_id' => 55, 'color_id' => 3, 'size' => 4, 'qty' => 10, 'price' => 200],
+
+            ['product_id' => 56, 'color_id' => 12, 'size' => 2, 'qty' => 10, 'price' => 120],
+            ['product_id' => 56, 'color_id' => 12, 'size' => 3, 'qty' => 10, 'price' => 120],
+            ['product_id' => 56, 'color_id' => 12, 'size' => 4, 'qty' => 10, 'price' => 120],
+
+            ['product_id' => 57, 'color_id' => 13, 'size' => 2, 'qty' => 10, 'price' => 100],
+            ['product_id' => 57, 'color_id' => 13, 'size' => 3, 'qty' => 10, 'price' => 100],
+            ['product_id' => 57, 'color_id' => 13, 'size' => 4, 'qty' => 10, 'price' => 100],
+
+            ['product_id' => 58, 'color_id' => 3, 'size' => 2, 'qty' => 10, 'price' => 250],
+            ['product_id' => 58, 'color_id' => 3, 'size' => 3, 'qty' => 10, 'price' => 250],
+            ['product_id' => 58, 'color_id' => 3, 'size' => 4, 'qty' => 10, 'price' => 250],
+            ['product_id' => 58, 'color_id' => 5, 'size' => 2, 'qty' => 10, 'price' => 250],
+            ['product_id' => 58, 'color_id' => 5, 'size' => 3, 'qty' => 10, 'price' => 250],
+            ['product_id' => 58, 'color_id' => 5, 'size' => 4, 'qty' => 10, 'price' => 250],
+
+        ]);
+    }
+}
